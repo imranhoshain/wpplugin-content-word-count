4fa096ca40b875323eca9a9d8c82b54b6f3fd884
Imran Hoshain <princ.imran@gmail.com>
2019-09-11 00:37:20 +0600

    action and filter hook for beginer

--- word-count.php
+++ word-count.php
@@ -29,9 +29,26 @@ add_action('plugin_loaded','wordcount_load_textdomain');
 function wordcount_count_words($content){
 	$stripped_content = strip_tags($content);
 	$word_length = str_word_count($stripped_content);
-	$label = __("Total Word is: ",'word-count');
-	$content .= sprintf('<h1>%s: %s</h1>',$label,$word_length);
+	$label = __("Total Word is",'word-count');
+	$label = apply_filters('wordcount_heading',$label);
+	$headtag = apply_filters( 'wordcount_tag','h1');
+	$content .= sprintf('<%s>%s: %s</%s>',$headtag,$label,$word_length,$headtag );
 	return $content;
 
 }
 add_filter('the_content','wordcount_count_words');
+
+
+//User interface || Use it function file
+function wordcount_head_change($heading){
+	$heading = "Count word is";
+	return $heading;
+}
+add_filter('wordcount_heading','wordcount_head_change');
+
+function wordcount_head_tag($headtag)
+{
+	$headtag = 'h2';
+	return $headtag;
+}
+add_filter('wordcount_tag','wordcount_head_tag');
